1338de13355ad9824926f79e7819f85e7d282d20
Julian <jpanucci@zola.com>
2023-02-06 13:25:49 -0600

    Rename to ZCharts

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Charts",
+    name: "ZCharts",
     platforms: [
           .iOS(.v12),
           .tvOS(.v12),
@@ -11,19 +11,19 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Charts",
-            targets: ["Charts"]),
+            name: "ZCharts",
+            targets: ["ZCharts"]),
         .library(
-            name: "ChartsDynamic",
+            name: "ZChartsDynamic",
             type: .dynamic,
-            targets: ["Charts"])
+            targets: ["ZCharts"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0")
     ],
     targets: [
         .target(
-            name: "Charts",
+            name: "ZCharts",
             dependencies: [.product(name: "Algorithms", package: "swift-algorithms")]
         )
     ],
